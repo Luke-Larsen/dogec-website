61c17c24ba983e3d6fdf1bb1395f3499f2fa5b91
Luke <luke@lukehanslarsen.com>
2021-04-27 10:27:49 -0400

    Panel 2

--- indext.php
+++ indext.php
@@ -6,57 +6,129 @@
 		<link href='assets0/css/main.css' rel="stylesheet">
 	</head>
 	<body>
-		<div class='main'>
-			<nav class="navbar navbar-expand-lg navbar-light bg-transparent">
-				<div class="container-fluid">
-					<a class="navbar-brand" href="#">
-						<img src="assets0/images/logo.svg" alt="" width="201" height="55" class="d-inline-block align-text-top">
-					</a>
-					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-						<span class="navbar-toggler-icon"></span>
-					</button>
-					<div class="collapse navbar-collapse" id="navbarNav">
-					<ul class="navbar-nav">
-						<li class="nav-item">
-						<a class="nav-link" aria-current="page" href="#">DogeCash</a>
-						</li>
-						<li class="nav-item">
-						<a class="nav-link" href="#">Merchants</a>
-						</li>
-						<li class="nav-item">
-						<a class="nav-link" href="#">Charities</a>
-						</li>
-						<li class="nav-item">
-						<a class="nav-link" href="#">News</a>
-						</li>
-						<li class="nav-item">
-						<a class="nav-link" href="#">Help</a>
-						</li>
-					</ul>
+		<div style='width:100%;height:100%;'>
+			<div class='main'>
+				<nav class="navbar navbar-expand-lg navbar-light bg-transparent">
+					<div class="container-fluid">
+						<a class="navbar-brand" href="#">
+							<img src="assets0/images/logo.svg" alt="" width="201" height="55" class="d-inline-block align-text-top">
+						</a>
+						<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+							<span class="navbar-toggler-icon"></span>
+						</button>
+						<div class="collapse navbar-collapse" id="navbarNav">
+						<ul class="navbar-nav">
+							<li class="nav-item">
+							<a class="nav-link" aria-current="page" href="#">DogeCash</a>
+							</li>
+							<li class="nav-item">
+							<a class="nav-link" href="#">Merchants</a>
+							</li>
+							<li class="nav-item">
+							<a class="nav-link" href="#">Charities</a>
+							</li>
+							<li class="nav-item">
+							<a class="nav-link" href="#">News</a>
+							</li>
+							<li class="nav-item">
+							<a class="nav-link" href="#">Help</a>
+							</li>
+						</ul>
+					</div>
+					<form class="d-flex justify-content-end">
+						<button class="btn btnTransparent" type="submit">English</button>
+						<i class="bi bi-twitter" style="font-size: 2rem; color: white;margin-left:17px;"></i>
+						<i class="bi bi-discord" style="font-size: 2rem; color: white;margin-left:16px;"></i>
+						<i class="bi bi-instagram" style="font-size: 2rem; color: white;margin-left:16px;"></i>
+					</form>
+				</nav>
+				<div style='margin-right:40%; margin-left:113px;margin-top:118px;'>
+					<h1 style='color:white;'>DogeCash is <br><span style='background: linear-gradient(45deg, #18171D, #CC8E58);background-clip: text;-webkit-background-clip: text;-moz-background-clip: text;-moz-text-fill-color: transparent;-webkit-text-fill-color: transparent;display: inline-block;'>Digital Cash</span></h1>
+					<h6 style='color:white;'>At DogeCash we are building a <br> unique ecosystem where your voice truly matters.</h6>
+					<div class='display:inline-block;'>
+						<button class="btn btnBrown" type="submit">Get Started</button>
+						<button class="btn btnTransparent" type="submit">Download Wallet</button>
+					</div>
 				</div>
-				<form class="d-flex justify-content-end">
-					<button class="btn btnTransparent" type="submit">English</button>
-					<i class="bi bi-twitter" style="font-size: 2rem; color: white;margin-left:17px;"></i>
-					<i class="bi bi-discord" style="font-size: 2rem; color: white;margin-left:16px;"></i>
-					<i class="bi bi-instagram" style="font-size: 2rem; color: white;margin-left:16px;"></i>
-				</form>
-			</nav>
-			<div style='margin-right:40%; margin-left:113px;margin-top:118px;'>
-				<h1 style='color:white;'>DogeCash is <br><span style='background: linear-gradient(45deg, #18171D, #CC8E58);background-clip: text;-webkit-background-clip: text;-moz-background-clip: text;-moz-text-fill-color: transparent;-webkit-text-fill-color: transparent;display: inline-block;'>Digital Cash</span></h1>
-				<h6 style='color:white;'>At DogeCash we are building a <br> unique ecosystem where your voice truly matters.</h6>
-				<div class='display:inline-block;'>
-					<button class="btn btnBrown" type="submit">Get Started</button>
-					<button class="btn btnWhite" type="submit">Download Wallet</button>
+				<div class="arrow">
+					<span></span>
+					<span></span>
 				</div>
 			</div>
-			<div class="arrow">
-                <span></span>
-                <span></span>
+			<div class='second'>
+				<h4 style='text-align:center;color:#FFF;'> All the hard work has been done for you </h4>
+				<h1 style='display: flex;justify-content: center;'class='gradient'> Your ready-to-go crypto</h1>
+				<div class="container" style='margin-top:61px;'>
+					<div class="row">
+						<div class="col infoBoxes">
+							<div class="row g-0 infoInner">
+								<div class="col">
+									<div style='width:208px;height:217px;background-color:red;'></div>
+								</div>
+								<div class="col">
+									<span style='font-size: 25px;'>Self-governed</h3>
+									<br>
+									<span style='font-size: 19px;'>DogeCash governance system allows masternodes holders to choose the path to go.</h5>
+									<button class="btn btnBrown" type="submit">Get Started</button>
+								</div>
+							</div>
+						</div>
+						<div class="col infoBoxes">
+							<div class="row g-0 infoInner">
+								<div class="col">
+									<div style='width:208px;height:217px;background-color:red;'></div>
+								</div>
+								<div class="col">
+									<span style='font-size: 25px;'>Private</h3>
+									<br>
+									<span style='font-size: 19px;'>With PIVX at its core, DogeCash cares about your privacy.</h5>
+									<button class="btn btnBrown" type="submit">Learn more</button>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="row" style='margin-top:61px;'>
+						<div class="col infoBoxes">
+							<div class="row g-0 infoInner">
+								<div class="col">
+									<div style='width:208px;height:217px;background-color:red;'></div>
+								</div>
+								<div class="col">
+									<span style='font-size: 25px;'>Green</h3>
+									<br>
+									<span style='font-size: 19px;'>DogeCash says no to PoW and adopts a model with zero emissions.</h5>
+									<button class="btn btnBrown" type="submit">Learn more</button>
+								</div>
+							</div>
+						</div>
+						<div class="col infoBoxes">
+							<div class="row g-0 infoInner">
+								<div class="col">
+									<div style='width:208px;height:217px;background-color:red;'></div>
+								</div>
+								<div class="col">
+									<span style='font-size: 25px;'>Fair</h3>
+									<br>
+									<span style='font-size: 19px;'>New DogeCash go directly to existing user and masternodes, rather than to wealthy miners.</h5>
+									<button class="btn btnBrown" type="submit">Learn more</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="third">
+				<div style='height: 699px;width: 1326px;'>
+					<div class="container">
+						<div class="row">
+							<div class="col">
+							</div>
+							<div class="col">
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
-		<section style='background-color:black;'>
-
-		</section>
-			
 	</body>
 </html>
